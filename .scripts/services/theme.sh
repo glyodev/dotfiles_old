d7f00d7b6435edffa72177ca10b0c6564e42666d
glyodev <admin@glyo.dev>
2026-05-07 21:49:46 -0400

    scripts

--- .scripts/services/theme.sh
+++ .scripts/services/theme.sh
@@ -21,10 +21,10 @@ TEMA_ACTUAL=$(grep '^gtk-theme-name=' "$HOME/.gtkrc-2.0" | cut -d '"' -f2)
 if [[ "$TEMA_ACTUAL" != "$NUEVO_TEMA" ]]; then
     # Actualizar tema en GTK 2
     sed -i "s/^gtk-theme-name=.*/gtk-theme-name=\"$NUEVO_TEMA\"/" "$HOME/.gtkrc-2.0"
-
+    
     # Actualizar tema en GTK 3
     sed -i "s/^gtk-theme-name=.*/gtk-theme-name=\"$NUEVO_TEMA\"/" "$HOME/.config/gtk-3.0/settings.ini"
-
+    
     # Recargar bspwm para aplicar el tema
     bspc wm -r
     echo "Tema cambiado a $NUEVO_TEMA y bspwm recargado."
